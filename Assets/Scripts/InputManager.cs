39fc9b48a4b0439b9fc7a87ec8e01a4891317fff
ql1232 <hoobooh48@gmail.com>
2025-03-02 01:14:15 -0800

    wasd movement

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour
+{
+public UnityEvent OnSpacePressed = new UnityEvent();
+public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+public UnityEvent OnResetPressed = new UnityEvent();
+void Update()
+{
+if (Input.GetKeyDown(KeyCode.Space))
+{
+OnSpacePressed?.Invoke();
+}
+Vector2 input = Vector2.zero;
+if (Input.GetKey(KeyCode.A))
+ {
+ input += Vector2.left;
+ }
+ if (Input.GetKey(KeyCode.D))
+ {
+ input += Vector2.right;
+ }
+if (Input.GetKey(KeyCode.W))
+ {
+ input += Vector2.up;
+ }
+if (Input.GetKey(KeyCode.S))
+ {
+ input += Vector2.down;
+ }
+ OnMove?.Invoke(input);
+}
+}	
