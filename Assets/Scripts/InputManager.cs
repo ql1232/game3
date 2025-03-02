caa291bf1486aef418989e2ee1006dafbb6de248
ql1232 <hoobooh48@gmail.com>
2025-03-02 05:39:02 -0800

    basic functionality
    works as intended.
    
    extremely difficult

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -9,6 +9,7 @@ public class InputManager : MonoBehaviour
 [SerializeField] private CinemachineCamera freeLookCamera;
 public UnityEvent OnSpacePressed = new UnityEvent();
 public UnityEvent<Vector3> OnMove = new UnityEvent<Vector3>();
+public UnityEvent OnJump = new UnityEvent();
 public UnityEvent OnResetPressed = new UnityEvent();
 void Update()
 {
@@ -27,12 +28,20 @@ if (Input.GetKey(KeyCode.A))
  }
 if (Input.GetKey(KeyCode.W))
  {
- input += Vector3.Normalize(freeLookCamera.transform.forward);
+	Vector3 v = freeLookCamera.transform.forward;
+	v.y=0;
+ input += Vector3.Normalize(v);
  }
 if (Input.GetKey(KeyCode.S))
  {
- input += Vector3.Normalize(freeLookCamera.transform.forward*-1);
+Vector3 v = freeLookCamera.transform.forward;
+	v.y=0;
+ input += Vector3.Normalize(v*-1);
  }
+if (Input.GetKey(KeyCode.Space))
+{
+ OnJump.Invoke();
+}
  OnMove?.Invoke(input);
 }
 }	
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,16 @@
  {
  [SerializeField] private InputManager inputManager;
  [SerializeField] private float speed;
+private int time = 0;
 
+[SerializeField] private bool b = false;
  private Rigidbody rb;
 
  private void Start()
  {
  //Adding MovePlayer as a listener to the OnMove event
  inputManager.OnMove.AddListener(MovePlayer);
+inputManager.OnJump.AddListener(JumpPlayer);
 
  rb = GetComponent<Rigidbody>();
  }
@@ -19,6 +22,26 @@
  //Only difference being, we only listen to Left and Right inputs
  private void MovePlayer(Vector3 direction)
  {
- rb.AddForce(5 * direction);
+Vector3 pos = transform.position;
+if(!Physics.CapsuleCast(transform.position,pos, 0.5F,new Vector3(0,-1,0),1, 1)){
+ rb.AddForce(1.5F * direction);
+}else{
+ rb.AddForce(4 * direction);}
 }
+ private void JumpPlayer()
+ {
+	Vector3 pos = transform.position;
+	pos.y-=0.5F;
+b=Physics.CapsuleCast(transform.position,pos, 0.5F,new Vector3(0,-1,0),1, 1);
+  if(b&&time==0){
+   rb.AddForce(new Vector3(0,600,0));
+	time=300;
+  }
+}
+public void Update(){
+ if(time>0){
+	time--;
+}
+}
+
 }
